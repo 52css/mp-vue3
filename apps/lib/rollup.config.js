51e14c06d3ec47d9b89636eda1b1492e2ff71d72
52css <jie_wei@126.com>
2024-08-04 20:46:37 +0800

    构建 1.0.5 只要1个包，不要依赖其他

--- apps/lib/rollup.config.js
+++ apps/lib/rollup.config.js
@@ -2,33 +2,38 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import replace from "@rollup/plugin-replace";
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.ts", // 入口文件
+  // external: ['@vue/reactivity'],
   output: [
     {
-      name: 'MpVue3',
+      name: "MpVue3",
       file: "dist/mp-vue3.min.js",
       format: "umd",
+      // sourcemap: true,
     },
   ],
   plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"), // 替换为 'production' 或 'development'
-      preventAssignment: true,
-    }),
-    resolve(), // 解析 Node.js 模块
-    commonjs(), // 将 CommonJS 模块转换为 ES6
+    resolve(), // 使 Rollup 能够查找并打包 node_modules 中的模块
+    commonjs(), // 将 CommonJS 模块转换为 ES6，以便 Rollup 可以处理它们
     typescript({
       tsconfig: "tsconfig.json", // 指定 tsconfig.json 文件
-      useTsconfigDeclarationDir: true
+      useTsconfigDeclarationDir: true,
+    }), // 支持 TypeScript
+    replace({
+      values: {
+        // __DEV__: false,
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      },
+      preventAssignment: true,
     }),
     babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**' // 仅编译我们的源代码
+      babelHelpers: "bundled",
+      exclude: "node_modules/**", // 仅编译我们的源代码
     }),
-    terser() // 压缩代码
+    terser(), // 压缩代码
   ],
 };
