770ff22469e32d00df5ae33163fcbb625f97473a
52css <jie_wei@126.com>
2024-08-27 15:28:53 +0800

    修改 @vue/reactivity 成 3.5版本，内部watch

--- apps/lib/types/scheduler.d.ts
+++
@@ -1,8 +0,0 @@
-export interface SchedulerJob extends Function {
-    active?: boolean;
-    allowRecurse?: boolean;
-}
-export declare function nextTick<R = void>(fn?: () => R): Promise<Awaited<R>>;
-export declare function queueJob(job: SchedulerJob): void;
-export declare function queuePostFlushCb(cb: SchedulerJob): void;
-export declare function flushPostFlushCbs(): void;
