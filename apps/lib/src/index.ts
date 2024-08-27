6e4725fa36c1d90ccea32a4a741f01cdd441d025
52css <jie_wei@126.com>
2024-08-27 15:47:57 +0800

    demo更新最新版本

--- apps/lib/src/index.ts
+++ apps/lib/src/index.ts
@@ -1,4 +1,61 @@
-export * from "@vue/reactivity";
+export {
+  // Core
+  computed,
+  reactive,
+  ref,
+  readonly,
+  // Utilities
+  unref,
+  proxyRefs,
+  isRef,
+  toRef,
+  toValue,
+  toRefs,
+  isProxy,
+  isReactive,
+  isReadonly,
+  isShallow,
+  // Advanced
+  customRef,
+  triggerRef,
+  shallowRef,
+  shallowReactive,
+  shallowReadonly,
+  markRaw,
+  toRaw,
+  // Effect
+  effect,
+  stop,
+  ReactiveEffect,
+  // Effect scope
+  effectScope,
+  getCurrentScope,
+  onScopeDispose,
+  // Types
+  type Ref,
+  type UnwrapRef,
+  type Reactive,
+  type ComputedRef,
+  type WritableComputedRef,
+  type ShallowRef,
+  type ToRefs,
+  // type Readonly,
+  type UnwrapNestedRefs,
+  type ShallowReactive,
+  type DeepReadonly,
+  type ToRef,
+  type ShallowUnwrapRef,
+  type CustomRefFactory,
+  type RefUnwrapBailTypes,
+  type Raw,
+  type TrackOpTypes,
+  type TriggerOpTypes,
+  type EffectScope,
+  type ReactiveMarker,
+  type DebuggerOptions,
+  watch,
+} from "@vue/reactivity";
+
 export * from "./component";
 export * from "./page";
 export * from "./lifetime";
